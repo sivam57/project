2b580ad4aca754a1124d3a9ad8584bc453eacafb
sivam57 <146501430+sivam57@users.noreply.github.com>
2026-02-12 15:40:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,59 +1,3 @@
-module "sg" {
-  source = "terraform-aws-modules/security-group/aws"
-
-  name        = "netflix-sg"
-  description = "Security group for netflix clone server"
-  vpc_id      = var.vpc_id
-
-  egress_with_cidr_blocks = [
-    {
-      from_port   = 8080
-      to_port     = 8080
-      protocol    = "tcp"
-      description = "Jenkins port"
-      cidr_blocks = "0.0.0.0/0"
-    },
-    {
-      from_port   = 443
-      to_port     = 443
-      protocol    = "tcp"
-      description = "HTTPS"
-      cidr_blocks = "0.0.0.0/0"
-    },
-    {
-      from_port   = 80
-      to_port     = 80
-      protocol    = "tcp"
-      description = "HTTP"
-      cidr_blocks = "0.0.0.0/0"
-    },
-    {
-      from_port   = 22
-      to_port     = 22
-      protocol    = "tcp"
-      description = "SSH"
-      cidr_blocks = "0.0.0.0/0"
-    },
-    {
-      from_port   = 9000
-      to_port     = 9000
-      protocol    = "tcp"
-      description = "SonarQube port"
-      cidr_blocks = "0.0.0.0/0"
-    }
-  ]
-
-  ingress_with_cidr_blocks = [
-    {
-      from_port   = 0
-      to_port     = 0
-      protocol    = "-1"
-      description = "All traffic"
-      cidr_blocks = "0.0.0.0/0"
-    }
-  ]
-}
-
 module "ec2_instance" {
   source = "terraform-aws-modules/ec2-instance/aws"
 
@@ -66,20 +10,17 @@ module "ec2_instance" {
   vpc_security_group_ids = [module.sg.security_group_id]
   subnet_id              = var.subnet_id
   user_data              = file("userdata.sh")
-  root_block_device = [
-    { volume_size = 25
-      volume_type = "gp3"
-    }
-  ]
+
+  root_block_device = [{
+    volume_size           = 25
+    volume_type           = "gp3"
+    delete_on_termination = true
+    encrypted             = true
+  }]
 
   tags = {
     Terraform   = "true"
     Environment = "dev"
     Name        = "netflix-server"
   }
-}
-
-resource "aws_eip" "eip" {
-  instance = module.ec2_instance.id
-  domain   = "vpc"
 }
